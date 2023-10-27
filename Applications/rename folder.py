25b069ff1c5d9f3d71fad36353b5ee3e0c01263b
Giuseppe Dotto <gppdtt@gmail.com>
2023-10-27 09:54:09 +0200

    rename folder function

--- Applications/rename folder.py
+++ Applications/rename folder.py
@@ -1,24 +1,35 @@
 
 import os
 
+def rename_nested(folder, suffix, add=True):
+    """
+    Order to follow to solve the case-sensitive issue:
+        1. run the function with add=True
+        2. commit the changes
+        3. run the function with add=False
+        4. commit for the last time
 
-def rename_nested(folder, addition, enum):
+    Args:
+        folder (str): the main folder to rename, with the nested as well
+        suffix (str): the unique string to add/remove from the path
+    """
+    amount = 0
     for dirpath, dirnames, filenames in os.walk(folder):
         for d in dirnames:
             complete_dir = os.path.join(dirpath, d)
-            # new_path = complete_dir+addition
-            new_path = complete_dir.replace(addition,'')
+            if add:
+                new_path = complete_dir+suffix
+            else:
+                new_path = complete_dir.replace(suffix,'')
             os.rename(complete_dir, new_path)
-            enum += 1
-            rename_nested(new_path, addition, enum)
+            amount += 1
+            amount += rename_nested(new_path, suffix, add)
         break
-
+    return  amount
 
 folder = os.path.dirname(__file__)
+suffix = '-----'
 
-enum = 0
-addition = '-----'
-
-rename_nested(folder, addition, enum)
-
-print(enum)
+# rename_nested(folder, suffix, enum)
+amount = rename_nested(folder, suffix, add=False)
+print(f'{amount} folders renamed successfully.')
